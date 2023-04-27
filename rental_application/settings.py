2be8232f3cdac1d4f481961788e4c8a63606b4fe
joel <joelvarughese257@gmail.com>
2023-04-27 15:06:37 +0530

    hosted backend

--- rental_application/settings.py
+++ rental_application/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY ='django-insecure-t9^pxze^4n$zjo0p1!84m0gh8h6fau11@16n*464(_w+)zdjgp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['127.0.0.1','https://rentalapplicationmanagement.onrender.com/','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','https://rentapp-jl2v.onrender.com','localhost']
 
 
 # Application definition
@@ -93,7 +93,7 @@ MIDDLEWARE = [
     'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    'https://rentalapplicationmanagement.onrender.com',
+    'https://rentapp-jl2v.onrender.com',
     "http://localhost:3000",
     "http://127.0.0.1:9000",
     "http://127.0.0.1:8000' "
